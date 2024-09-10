66c95dbd93d38609499eaff3e36710ab67d7467d
Shelby Myrman <myrmanshelby@gmail.com>
2024-09-10 13:05:26 -0700

    Fixed filepath

--- flaskapplication/lang_detection.py
+++ flaskapplication/lang_detection.py
@@ -191,7 +191,7 @@ def preprocess_text(paragraph):
     return tokenized_lines
 
 def test_language(text):
-    directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "dataset/nGrams/")
+    directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "dataset/ngrams/")
     trained_languages = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith('.nGrams.txt')]
     tokenized_lines = preprocess_text(text)
     n_gram_counts = collections.Counter()
